d29ea89f2b7c92017125979b4285da686d6d7369
taeheon <dqrd123@gmail.com>
2020-02-29 13:56:00 +0900

    feat: 당첨 갯수 Count 클래스 구현

---
+++ src/main/java/lotto/domain/result/Count.java
@@ -0,0 +1,13 @@
+package lotto.domain.result;
+
+public class Count {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void addCount() {
+        count++;
+    }
+}
